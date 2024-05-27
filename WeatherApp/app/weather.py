13cdf80bb81e1875194b0dc37f595559240afd11
root <admin@example.com>
2024-05-27 13:44:42 +0300

    /k

--- WeatherApp/app/weather.py
+++ WeatherApp/app/weather.py
@@ -7,7 +7,6 @@ import json
 from datetime import datetime
 import os
 
-
 load_dotenv()
 app = Flask(__name__)
 
@@ -16,12 +15,9 @@ api_key = os.getenv('KEY_API')
 history_dir = 'search_history'
 os.makedirs(history_dir, exist_ok=True)  # Ensure the directory exists
 
-
 @app.route('/', methods=['GET', 'POST'])
 def get_input():
     json_data = {}
-
-
     if request.method == 'POST':
         user_input = request.form['user_input']
         url = (
@@ -29,8 +25,6 @@ def get_input():
             f"rest/services/timeline/{user_input}/next7days?unitGroup=metric&"
             f"key={api_key}&include=days&land=en&iconSetvalues=‘icons1’"
         )
-        
-        
         try:
             response = requests.get(url, timeout=10)
             print(f"Response Status Code: {response.status_code}")
@@ -38,8 +32,6 @@ def get_input():
                 json_data = response.json()
                 # Save the search to history
                 save_search_to_history(user_input, json_data)
-            
-            
             else:
                 print(
                     f"Error retrieving data. Status code: "
@@ -53,7 +45,6 @@ def get_input():
         'input_form.html', api_data=json_data, bg_color=bg_color
     )
 
-
 @app.route('/history')
 def show_history():
     # List all the files in the history directory
@@ -62,16 +53,13 @@ def show_history():
     files.sort(reverse=True)  # Sort files by date
     return render_template('history.html', files=files)
 
-
 @app.route('/download/<filename>')
 def download_file(filename):
     return send_from_directory(history_dir, filename, as_attachment=True)
 
-
 @app.route('/error', methods=['GET'])
 def error_page():
     return render_template('error.html')
-
 
 def save_search_to_history(city, data):
     # Include the date and city in the data to be saved
@@ -88,7 +76,6 @@ def save_search_to_history(city, data):
     file_path = os.path.join(history_dir, filename)
     with open(file_path, 'a') as f:
         json.dump(history_data, f, indent=4)
-
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
