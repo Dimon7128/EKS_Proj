9c12e5d39384855dd765af2a409eb6006cd48a7b
root <admin@example.com>
2024-05-28 22:31:58 +0300

    changing the docker file

--- WeatherApp/app/pytest.py
+++ WeatherApp/app/pytest.py
@@ -4,14 +4,14 @@ import requests
 
 APP_ADDR = "http://13.38.244.199/"
 
-# Asserts a 200 response code from the weather web app using requests module
+# Asserts a 200 response code from the weather  web app using requests module
 
 
 class AppConectivityTest(unittest.TestCase):
 
     def test_connection(self):
         response = requests.get(APP_ADDR, timeout=10).status_code
-        self.assertEqual(response, 200, "Web app is not reachable:( )")
+        self.assertEqual(response, 200, "Web app  is not reachable:( )")
 
 
 if __name__ == "main":
