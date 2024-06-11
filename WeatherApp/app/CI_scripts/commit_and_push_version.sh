daedcf9ae7c2cc3ee4fdf5114b1758e9f42d4d97
root <admin@example.com>
2024-06-11 18:02:15 +0300

    the pull is inserted inside the 'commit_and_push_script'

--- WeatherApp/app/CI_scripts/commit_and_push_version.sh
+++ WeatherApp/app/CI_scripts/commit_and_push_version.sh
@@ -4,3 +4,6 @@ git config --global user.email "github-actions@github.com"
 git add WeatherApp/app/VERSION
 git commit -m "Bump version to $new_version"
 git push
+
+#Pull the latest changes to ensure the local repo is up to date
+git pull origin main
